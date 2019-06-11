b6dc44a5f689f7b431ccd0b542cbc4ef009fe080
huggeri <katq228@gmail.com>
2019-06-12 02:13:21 +0700

    правка карты

--- src/js/map-settings.js
+++ src/js/map-settings.js
@@ -1,41 +1,12 @@
-ymaps.ready(init);
-
-function init(){
-  var myMap;
-  $('.map').hover(function() {
-    if(!myMap) {
-      console.log('жопа');
-      myMap = new ymaps.Map("map", {
-      center: [55.61140907, 37.20112250],
-      zoom: 16,
-      });
-          
-      myMap.behaviors.disable('scrollZoom');
-      
-      myPlacemark = new ymaps.Placemark([55.61140907, 37.20112250], {
-        hintContent: 'Ремонт квартиир',
-        balloonContent: 'ул. Ленина, 10',
-        }, {
-            // Опции.
-            // Необходимо указать данный тип макета.
-            iconLayout: 'default#image',
-            // Своё изображение иконки метки.
-            iconImageHref: 'img/footer/geo.png',
-            // Размеры метки.
-            iconImageSize: [25, 25],
-            // Смещение левого верхнего угла иконки относительно
-            // её "ножки" (точки привязки).
-            iconImageOffset: [-11.5, -28],
-          });
-          
-      myMap.geoObjects.add(myPlacemark);
+$(document).ready(function() {
+  $(window).scroll(function () {
+    if($(window).scrollTop() > 500) {
+      if($(window).width() > 567) {
+        $('.map > script').attr('src', 'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A41fb8bc8252dc6c323f4525a235a1b150931393704855c1900cfbad12b58e8c6&amp;width=100%25&amp;height=640&amp;lang=ru_RU&amp;scroll=false');
+      }
+      else {
+        $('.map > script').attr('src', 'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A41fb8bc8252dc6c323f4525a235a1b150931393704855c1900cfbad12b58e8c6&amp;width=100%25&amp;height=665&amp;lang=ru_RU&amp;scroll=false');
+      }
     }
   });
-}
-
-if($(window).width() > 567) {
-  $('.map').attr('style', 'height: 640px');
-}
-else {
-  $('.map').attr('style', 'height: 665px');
-}
+});
