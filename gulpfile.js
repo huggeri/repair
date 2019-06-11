f139f226291ee8b64f2b1e164dbd9f361d413726
huggeri <katq228@gmail.com>
2019-06-12 00:00:09 +0700

    Подключение карт через скриптб минимизация некоторых файлов

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp');
 var cleanCss = require('gulp-clean-css');
-var cleanJs = require('gulp-file-inline');
 var htmlMin = require('gulp-htmlmin');
 var tinyPNG = require('gulp-tinypng-compress');
+var minifyjs = require('gulp-js-minify');
 
 function defaultTask(cb) {
   // place code for your default task here
@@ -10,9 +10,17 @@ function defaultTask(cb) {
 }
 
 exports.default = defaultTask
+
+//сжимаем js.файлы
+gulp.task('minify-js', function(){
+  gulp.src('./src/js/main.js')
+    .pipe(minifyjs())
+    .pipe(gulp.dest('./dist/js'));
+});
+
 // cжимаем css
 gulp.task('minify-css', function(cb) {
-  gulp.src('.src/css/*.css')
+  gulp.src('src/css/*.css')
   .pipe(cleanCss ({
     compatibility: 'ie8'
   }))
@@ -21,26 +29,17 @@ gulp.task('minify-css', function(cb) {
   cb();
 });
 
-// cжимаем js
-gulp.task('minify-js', function(cb) {
-  gulp.src('.src/js/main.js')
-  .pipe(cleanJs ())
-  .pipe(gulp.dest('dist/js'))
-
-  cb();
-});
-
 // переносим сжатые бибилиотеки в dist
 gulp.task('move-js', function(cb) {
-  return gulp.src('.src/js/*.min.js')
-  .pipe(gulp.dest('dist/js/'))
+  return gulp.src('./src/js/*.js')
+  .pipe(gulp.dest('dist/js'))
 
   cb();
 });
 
 // cжимаем html файл
 gulp.task('minify-html', function(cb) {
-  gulp.src('.src/*.html')
+  gulp.src('./src/*.html')
   .pipe(htmlMin ({
     collapseWhitespace: true
   }))
@@ -51,7 +50,7 @@ gulp.task('minify-html', function(cb) {
 
 // перекидываем шрифты
 gulp.task('move-fonts', function(cb) {
-  return gulp.src('.src/fonts/**/*.html')
+  return gulp.src('src/fonts/**/*.*')
   .pipe(gulp.dest('dist/fonts'))
 
   cb();
@@ -60,7 +59,7 @@ gulp.task('move-fonts', function(cb) {
 var tinypng = require('gulp-tinypng-compress');
  
 gulp.task('tinypng', function (cb) {
-  gulp.src('../src/img/**/*.{png,jpg,jpeg}')
+  gulp.src('src/img/**/*.{png,jpg,jpeg}')
     .pipe(tinyPNG({
         key: 'HPULULvNdCHMLxBYmK6evKEyS719KzXe'
     }))
